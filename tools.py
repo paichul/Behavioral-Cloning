ffa63b7d0f7bfd3118bedc93732f31f4b12cfa66
Paichun Jim Lin <jimlinfeeling@gmail.com>
2017-12-28 21:52:17 -0800

    Delete tools.py

--- tools.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def preprocess(image):
-  yuv = cv2.cvtColor(image,cv2.COLOR_BGR2YUV) #convert from BGR to YUV
-  hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-  yuv[:,:,2] = hsv[:,:,2] #replace yuv's v channel with hsv's v channel
-  image = image[50:-20, 30:-30,:] #cropping
-  return image
