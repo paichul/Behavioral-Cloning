e23644166d362054bef2ff45e6e9b36efe30adbe
Paichun Jim Lin <jimlinfeeling@gmail.com>
2018-01-02 15:22:19 -0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -13,9 +13,6 @@ def get_samples(filename):
 
 #data preprocessing
 def preprocess(image):
-  yuv = cv2.cvtColor(image,cv2.COLOR_BGR2YUV) #convert from BGR to YUV
-  hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-  yuv[:,:,2] = hsv[:,:,2] #replace yuv's v channel with hsv's v channel
   image = image[50:-20, 30:-30,:] #cropping
   image = image/127.5 - 1.0 #normalization
   return image
